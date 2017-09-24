2df45b561d7334cbe997f0f3329ff07d918c03d3
sibisalu <sibinasalih@gmail.com>
2017-09-24 11:32:47 +0530

    Initial commit

---
+++ TrackingApplication/app/src/main/java/sibina/com/trackingapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package sibina.com.trackingapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
